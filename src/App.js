cffaecbc5a2bfb14361b5e5a0f46203181dd44f1
junglimit <jaehan_jjh@naver.com>
2024-07-26 09:12:01 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+import Counter from './optimizing/components/Counter/Counter';
+import Header from './optimizing/components/Header';
+import { log } from './optimizing/log';
+import ConfigureCounter from './optimizing/components/Counter/ConfigureCounter';
+
+const App = () => {
+  log('<App /> rendered');
+
+  const [chosenCount, setChosenCount] = useState(0);
+
+  const setCountHandler = number => {
+    setChosenCount(number);
+  };
+  
+  return (
+    <>
+      <Header />
+      <main>
+        <ConfigureCounter onSet={setCountHandler} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
+      </main>
+    </>
+  );
+};
+
+export default App;
